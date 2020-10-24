53ef9b6bd75a31b77e5a319f68c97f8a3ccbb217
Charles Forsyth <charles.forsyth@ucr.edu>
2020-10-23 23:02:49 -0700

    blast

---
+++ install.sh
@@ -0,0 +1,3 @@
+#! /bin/bash -l
+
+sudo yum install -y wget && wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/002/994/505/GCA_002994505.1_ASM299450v1/GCA_002994505.1_ASM299450v1_genomic.fna.gz && gunzip GCA_002994505.1_ASM299450v1_genomic.fna.gz && wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.10.1+-x64-linux.tar.gz && tar -zxvf ncbi-blast-2.10.1+-x64-linux.tar.gz && ./ncbi-blast-2.10.1+/bin/makeblastdb -in GCA_002994505.1_ASM299450v1_genomic.fna -dbtype nucl -input_type fasta -out SerRivdb && tblastn -db SerRivdb -query benedeniaGene.fasta  -out blastout.txt
