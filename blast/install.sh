ccb36f55f810e73eadec7a8adce7aab2f15f0a63
Charles Forsyth <charles.forsyth@ucr.edu>
2020-10-24 00:29:45 -0700

    good

--- blast/install.sh
+++ blast/install.sh
@@ -2,5 +2,8 @@
 
 cd /opt/repo/blast
 
-wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.10.1+-x64-linux.tar.gz &&
+wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.10.1+-x64-linux.tar.gz 
 tar -zxvf ncbi-blast-2.10.1+-x64-linux.tar.gz
+
+ls -latr
+
