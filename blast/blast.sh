2621ffd3227e386cbe6479d71197857579460ea6
Charles Forsyth <charles.forsyth@ucr.edu>
2020-10-24 19:42:31 -0700

    good

--- blast/blast.sh
+++ blast/blast.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -l
 
-cp /opt/repo/blast/blast/* .
+## Copy job files to working directory ##
+cp /opt/repo/nbt/blast/* .
 
-~/ncbi-blast-2.10.1+/bin/makeblastdb -in mouse.1.protein.faa -dbtype prot
-~/ncbi-blast-2.10.1+/bin/makeblastdb -in zebrafish.1.protein.faa -dbtype prot
 
-~/ncbi-blast-2.10.1+/bin/blastp -query zebrafish.1.protein.faa -db mouse.1.protein.faa -num_threads 4 -out zebrafish.x.mouse
+makeblastdb -in mouse.1.protein.faa -dbtype prot
+makeblastdb -in zebrafish.1.protein.faa -dbtype prot
+
+blastp -query zebrafish.1.protein.faa -db mouse.1.protein.faa -num_threads 4 -out zebrafish.x.mouse
--- blast/install.sh
+++ blast/install.sh
@@ -1,6 +1,10 @@
 #! /bin/bash -l
 
+
+## Download and Install Software ##
 wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.10.1+-x64-linux.tar.gz 
 tar -zxvf ncbi-blast-2.10.1+-x64-linux.tar.gz
+cp ncbi-blast-2.10.1+/bin/* /usr/bin/.
 
-
+## Copy job files to working directory ##
+#cp /opt/repo/nbt/blast/* .
