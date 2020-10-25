bad485c769b6845304969f2323e84f31fdc304b3
Charles Forsyth <charles.forsyth@ucr.edu>
2020-10-25 13:17:38 -0700

    good

--- blast/blast.sh
+++ blast/blast.sh
@@ -7,4 +7,9 @@ cp /opt/repo/nbt/blast/* .
 ./makeblastdb -in mouse.1.protein.faa -dbtype prot
 ./makeblastdb -in zebrafish.1.protein.faa -dbtype prot
 
-./blastp -query zebrafish.1.protein.faa -db mouse.1.protein.faa -num_threads 4 -out zebrafish.x.mouse
+#./blastp -query zebrafish.1.protein.faa -db mouse.1.protein.faa -num_threads 4 -out zebrafish.x.mouse
+./blastp -query zebrafish.top.faa -db mouse.1.protein.faa -num_threads 4 -out zebrafish.x.mouse
+
+tar -zxvf job-output.tar.gz zebrafish.x.mouse
+cp job-output.tar.gz /sharedvol/.
+
