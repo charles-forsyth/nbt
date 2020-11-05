9cecba1b12ae065630a1dd48d7721bbfca7f90aa
Charles Forsyth <charles.forsyth@ucr.edu>
2020-11-04 17:38:21 -0800

    changed the blast url to static 2.10.1

--- blast/install.sh
+++ blast/install.sh
@@ -2,7 +2,7 @@
 
 
 ## Download and Install Software ##
-wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.10.1+-x64-linux.tar.gz 
+wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.10.1/ncbi-blast-2.10.1+-x64-linux.tar.gz
 tar -zxvf ncbi-blast-2.10.1+-x64-linux.tar.gz
 cp ncbi-blast-2.10.1+/bin/* .
 
